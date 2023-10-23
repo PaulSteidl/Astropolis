63e391851f345c1f03ecaa5207f72a57a8988476
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2023-10-23 15:39:35 +0200

    .

---
+++ Assets/MyGame/Scripts/Rocket/RocketManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketManager : MonoBehaviour
+{
+    public float moneyPerPerson;
+    public float money;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
