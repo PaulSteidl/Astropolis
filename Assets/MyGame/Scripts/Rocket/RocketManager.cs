33e24499adee68599885aa82d7750137a4f1e868
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2023-10-24 20:02:54 +0200

    .

--- Assets/MyGame/Scripts/Rocket/RocketManager.cs
+++ Assets/MyGame/Scripts/Rocket/RocketManager.cs
@@ -4,18 +4,67 @@ using UnityEngine;
 
 public class RocketManager : MonoBehaviour
 {
+    public float moneyMade;
     public float moneyPerPerson;
-    public float money;
+    public float peoplePerRocket;
+    public float rocketComebackTime;
+    public float rocketTakeOffTime;
 
+
+    [SerializeField] bool rocketStarted, automaticAllowed;
 
     void Start()
     {
-        
+        StartCoroutine("TakeOff");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeOffOnTouch(); //testzwecke  touch simu        
+        }
+
+        if (rocketStarted)
+        {
+            automaticAllowed = false;
+        }
+    }
+    public void TakeOffOnTouch()
+    {       
+        if (!rocketStarted)                                 //Touch Start
+        {
+            StartCoroutine("TakeOff");        
+        }
+    }
+
+    public IEnumerator TakeOff()
+    {
+        rocketStarted = true;
+        yield return new WaitForSeconds(rocketComebackTime);    // Flug Zeit von Rackete
+        moneyMade += MoneyPerRocket();
+        rocketStarted = false;
+
+
+        //automaticAllowed = true;
         
+        
+        //yield return new WaitForSeconds(rocketTakeOffTime);   //automatische Start Zeit    
+
+        
+       
+        
+        //if (automaticAllowed)
+        //{
+        //    StartCoroutine("TakeOff");    
+        //}      
+    }
+
+
+    float MoneyPerRocket()
+    {
+        float moneyMadeRocket = moneyPerPerson * peoplePerRocket;
+        return moneyMadeRocket;
     }
 }
