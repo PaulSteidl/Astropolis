174af5cbb915a5b7298b86891a9148ae2a12e2f6
Paul Steidl <62242723+PaulSteidl@users.noreply.github.com>
2023-10-24 20:35:13 +0200

    .

--- Assets/MyGame/Scripts/Camera/CameraManager.cs
+++ Assets/MyGame/Scripts/Camera/CameraManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class CameraManager : MonoBehaviour
 {
 
-    public Vector2 _delta;
+	public float orthoZoomSpeed;
+
+	public Vector2 _delta;
 
     public bool _isMoving;
 
@@ -31,5 +33,33 @@ public class CameraManager : MonoBehaviour
             transform.position += position * Time.deltaTime;
         }
 
-    }
+		// If there are two touches on the device...
+		if (Input.touchCount == 2)
+		{
+			// Store both touches.
+			Touch touchZero = Input.GetTouch(0);
+			Touch touchOne = Input.GetTouch(1);
+
+			// Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			// Find the magnitude of the vector (the distance) between the touches in each frame.
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			// Find the difference in the distances between each frame.
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+			// If the camera is orthographic...
+			if (GetComponent<Camera>().orthographic)
+			{
+				// ... change the orthographic size based on the change in distance between the touches.
+				GetComponent<Camera>().orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+				// Make sure the orthographic size never drops below zero.
+				GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);
+			}
+		}
+	}
 }
