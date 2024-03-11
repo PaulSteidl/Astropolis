27fe6219bc63be64c50ceb4467774c1b82c272ba
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2024-03-11 14:32:26 +0100

    .

--- Assets/MyGame/Scripts/Asteroids/AsteroidsManager.cs
+++ Assets/MyGame/Scripts/Asteroids/AsteroidsManager.cs
@@ -14,12 +14,12 @@ public class AsteroidsManager : MonoBehaviour
     public IEnumerator Asteroid()
     {
         GameObject g = Instantiate(asteroid, new Vector3(0, 0, 0), Quaternion.identity);
-        //g.transform.position = new Vector3(0, 0, 0);
+        g.transform.position = new Vector3(0, 0, 0);
         g.transform.SetParent(asteroidManager.transform);
         //float a = camera1.transform.position.x + 500;
         //gameObject.transform.Find("Image").position = new Vector3(a, 0, 0);
         
-        //g.transform.localPosition = new Vector3(0, 0, 0);
+        g.transform.localPosition = new Vector3(0, 0, 0);
         g.transform.localRotation = new Quaternion(0, 0, 0, 0);
         g.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         yield return new WaitForSeconds(1);
